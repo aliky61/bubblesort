dfd1012fe939aabaedbd2478a5e32958e7162c7b
Kaya <a.kaya@accenture.com>
2020-02-10 14:38:28 +0100

    blubb

---
+++ src/main/java/com/acn/BubbleSort.java
@@ -0,0 +1,19 @@
+package com.acn;
+
+public class BubbleSort {
+
+	public static int[] bubblesort(int[] unsortedArray) {
+		int temp;
+		for (int i = 1; i < unsortedArray.length; i++) {
+			for (int j = 0; j < unsortedArray.length - i; j++) {
+				if (unsortedArray[j] > unsortedArray[j + 1]) {
+					temp = unsortedArray[j];
+					unsortedArray[j] = unsortedArray[j + 1];
+					unsortedArray[j + 1] = temp;
+				}
+
+			}
+		}
+		return unsortedArray;
+	}
+}
